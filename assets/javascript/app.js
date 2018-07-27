219213177a39126eafa80a3105aaf904152c41eb
Rumble Roar <lnyopp15@gmail.com>
2018-07-27 14:13:44 -0400

    i got buttonsgit add .

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var question =[ // array of questions with answers.
+
+]
+var score = 0;
+
+// start button.
+$("button").click(function() 
+{ 
+
+}
+
+// timer
+// create form for questions.
+// create second page for results.
+
